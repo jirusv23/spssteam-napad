ce54f9345260e5d11420897f531e3c23f32a50c3
Vojtěch Jiruš <jirusv23@spstrutnov.cz>
2024-04-06 20:41:53 +0200

    sadsadsa

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import pygame, os, sys
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 #open the window in the middle of the screen
 
-pygame.init()
-Verdana29 = pygame.font.SysFont("Verdana", 29)
-Verdana22 = pygame.font.SysFont("Verdana", 22)
-
 #sets resolution
 rozliseniObrazovky = (800,1000)
 
 okno = pygame.display.set_mode(rozliseniObrazovky)
 pygame.display.set_caption("SPŠsteam") #capiton on top left
 
-#print(pygame.font.get_fonts())
-#prints all availible fonts
+
+#sets fonts
+pygame.init()
+Verdana29 = pygame.font.SysFont("Verdana", 29)
+Verdana22 = pygame.font.SysFont("Verdana", 22)
+
+''' print(pygame.font.get_fonts())
+    prints all availible fonts  '''
+
+#Images
+profilovka = pygame.image.load("C:/Users/Ok I pull up/Desktop/vj/spssteam-napad/profilePicture.png")
+profilovka = pygame.transform.scale(okno, (100,100))
 
 run = True
 while run:
@@ -23,10 +29,14 @@ while run:
             run = False
             sys.exit()
 
+    stisknuteKlavesy = pygame.key.get_pressed()
+    mousePosition = pygame.mouse.get_pos()
+
     okno.fill((102,140,244))
 
     topBar = pygame.draw.rect(okno, (42,71,94), (0,0, rozliseniObrazovky[1], 110))
 
+    #Renders buttons
     listHerButton = pygame.draw.rect(okno, (24,48,66), (30,30, 150,50), 2, 1) #draws box for listHer
     listHerText = Verdana29.render("Knihovna", False, (0,0,0)) #renders text
 
@@ -36,6 +46,10 @@ while run:
     ObchodButton = pygame.draw.rect(okno, (24,48,66), (390,30, 150,50), 2, 1) 
     obchodText = Verdana29.render("Obchod", True, (0,0,0))
 
+    #TopBar Profile
+    okno.blit(profilovka, (560, 5))
+
+    #RENDERS FONT
     okno.blit(listHerText, (38,37)) #blit listHer text
     okno.blit(leaderboardText, (216, 41))
     okno.blit(obchodText, (407, 37))
